f52a596ebb4fd0d76e890dee530b806c67505938
ankur srivastava <ankur.srivastava@techchefz.com>
2025-12-31 16:27:15 +0530

    hero banner/gaming experience

--- components/organisms/gamingExperience/index.jsx
+++ components/organisms/gamingExperience/index.jsx
@@ -6,11 +6,11 @@ import React, { useEffect, useState } from "react"
 const GamingExperience = () => {
   const [gamingExperience, setGamingExperience] = useState(null)
 
-  useEffect (()=>{
+  useEffect(() => {
     async function getGamingExperience() {
-      const res = await fetch (
+      const res = await fetch(
         "http://localhost:1337/api/landing-page?populate[gamingExperience][populate]=*",
-        {cache:"no-store"}
+        { cache: "no-store" }
       )
       const data = await res.json()
       setGamingExperience(data)
@@ -19,30 +19,35 @@ const GamingExperience = () => {
       console.log(`"Image2 URL:", http://localhost:1337${data?.data?.gamingExperience?.image2?.url}`)
     }
     getGamingExperience();
-  },[])
-  
+  }, [])
+  const gameData = gamingExperience?.data?.gamingExperience
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-between md:pl-20 py-14 md:py-0 bg-[#E6E9F2] my-16 rounded-xl overflow-hidden">
-      <CustomImage
-        src={`http://localhost:1337${gamingExperience?.data?.gamingExperience?.image1.url}`}
-        width={1000}
-        height={1000}
-        className="max-w-56"
-      />
+      {gameData?.image1?.url && (
+        <CustomImage
+          src={`http://localhost:1337${gameData?.image1?.url}`}
+          width={1000}
+          height={1000}
+          className="max-w-56"
+        />
+      )}
+
       <div className="flex flex-col items-center justify-center text-center">
-        <h2 className="text-2xl md:text-3xl font-bold max-w-[290px]">{gamingExperience?.data?.gamingExperience?.title}</h2>
-        <p className="max-w-[343px] font-medium text-gray-800/60 my-3">{gamingExperience?.data?.gamingExperience?.description}</p>
-        <Link href={gamingExperience?.data?.gamingExperience?.link?.href || "#"}>
-          <button className="flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">{gamingExperience?.data?.gamingExperience?.link?.text}</button>
+        <h2 className="text-2xl md:text-3xl font-bold max-w-[290px]">{gameData?.title}</h2>
+        <p className="max-w-[343px] font-medium text-gray-800/60 my-3">{gameData?.description}</p>
+        <Link href={gameData?.href || "#"}>
+          <button className="flex items-center justify-center gap-1 px-12 py-2.5 bg-orange-600 rounded text-white">{gameData?.link?.text}</button>
         </Link>
       </div>
-      <CustomImage
-        src={`http://localhost:1337${gamingExperience?.data?.gamingExperience?.image2.url}`}
-        width={1000}
-        height={1000}
-        className="hidden md:block max-w-80"
-      />
+      {gameData?.image2?.url && (
+        <CustomImage
+          src={`http://localhost:1337${gameData?.image2?.url}`}
+          width={1000}
+          height={1000}
+          className="hidden md:block max-w-80"
+        />
+      )}
     </div>
   )
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+}
 
-    images: {
-      domains: ["localhost"],
-    },
-
-};
-
-export default nextConfig;
+export default nextConfig
